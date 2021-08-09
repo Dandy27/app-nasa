dcee43a18997bed06fe547479531ffb6665ae6d3
dandy <danielsbarbosa27@gmail.com>
2021-08-09 14:54:06 -0300

    Entity

---
+++ clean_architecture/lib/features/domain/entities/space_media_entity.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+
+class SpaceMediaEntity extends Equatable {
+  final String copyright;
+  final String date;
+  final String explanation;
+  final String hdurl;
+  final String mediaType;
+  final String serviceVersion;
+  final String title;
+  final String url;
+
+  SpaceMediaEntity(
+      {required this.copyright,
+      required this.date,
+      required this.explanation,
+      required this.hdurl,
+      required this.mediaType,
+      required this.serviceVersion,
+      required this.title,
+      required this.url});
+  @override
+  List<Object?> get props => [
+        copyright,
+        date,
+        explanation,
+        hdurl,
+        mediaType,
+        serviceVersion,
+        title,
+        url
+      ];
+}
